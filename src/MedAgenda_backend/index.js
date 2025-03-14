441da9f40c3f1f03eb3ec5eef5f4567d5e71ea1f
maur32 <maur.martins32@gmail.com>
2025-03-14 17:41:12 -0300

    Configuração inicial backend

---
+++ src/MedAgenda_backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const authRouter = require("./routers/authRouter");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(express.urlencoded({extended: true}));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/auth", authRouter);
+app.get("/", (req, res) => {
+  res.json({message: "Hello World!"});
+});
+
+app.listen(process.env.PORT);
