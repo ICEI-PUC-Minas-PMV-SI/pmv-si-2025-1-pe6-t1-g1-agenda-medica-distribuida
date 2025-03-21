3fcf0d25b0a384d8cc44c6cc1c4897b0daa1f6d2
maur32 <maur.martins32@gmail.com>
2025-03-21 18:59:56 -0300

    Adicionando autenticação

--- src/MedAgenda_backend/middlewares/validator.js
+++ src/MedAgenda_backend/middlewares/validator.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const joi = require("joi");
 
 exports.signupSchema = joi.object({
@@ -11,6 +12,57 @@ exports.signupSchema = joi.object({
     }),
   password: joi
     .string()
+    .required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")),
+});
+
+exports.signinSchema = joi.object({
+  email: joi
+    .string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: {allow: ["com", "net"]},
+    }),
+  password: joi
+    .string()
+    .required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")),
+});
+
+exports.acceptCodeSchema = Joi.object({
+  email: joi
+    .string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: {allow: ["com", "net"]},
+    }),
+  providedCode: joi.number(),
+});
+
+exports.changePasswordSchema = Joi.object({
+  newPassword: Joi.string()
+    .required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")),
+  oldPassword: Joi.string()
+    .required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")),
+});
+
+exports.acceptForgotPasswordCodeSchema = Joi.object({
+  email: joi
+    .string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: {allow: ["com", "net"]},
+    }),
+  providedCode: joi.number(),
+  newPassword: Joi.string()
     .required()
     .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")),
 });
