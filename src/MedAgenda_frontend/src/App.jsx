47bf67be412d998d7b827acf307c95e4fde47569
rodrigopenafurt <rodfpar@hotmail.com>
2025-04-24 18:08:47 -0300

    app

--- src/MedAgenda_frontend/src/App.jsx
+++ src/MedAgenda_frontend/src/App.jsx
@@ -23,9 +23,7 @@ const App = () => {
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
-
       </Routes>
-      teste
     </div>
   )
 }
