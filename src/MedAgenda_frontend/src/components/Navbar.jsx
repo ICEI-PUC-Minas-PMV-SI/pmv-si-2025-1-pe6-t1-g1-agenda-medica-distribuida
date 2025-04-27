8bd1ff14f7a0c738cef3e0abb0d1158c591ddb75
rodrigopenafurt <rodfpar@hotmail.com>
2025-04-27 10:58:09 -0300

    nav atualizada

--- src/MedAgenda_frontend/src/components/Navbar.jsx
+++ src/MedAgenda_frontend/src/components/Navbar.jsx
@@ -4,28 +4,28 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]'>
-        <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+    <div className='flex items-center justify-between text-sm py-4 mb-5'>
+        <img className='w-60 cursor-pointer' src={assets.logo} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
             <NavLink to='/doctors'>
                 <li className='py-1'>DOUTORES</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
             <NavLink to='/about'>
                 <li className='py-1'>SOBRE NÓS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
             <NavLink to='/contact'>
                 <li className='py-1'>CONTATO</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
         </ul>
         <div>
-            <button>Crie sua conta</button>
+            <button className='bg-black text-white px-8 py-3 rounded-full font-light hidden md:block'>Crie sua conta</button>
         </div>
     </div>
   )
