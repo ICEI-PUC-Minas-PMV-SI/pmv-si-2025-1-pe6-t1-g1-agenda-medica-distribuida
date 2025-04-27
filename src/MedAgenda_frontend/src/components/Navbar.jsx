a7ef24e31942aad689ff92f97c76bfd53015ce54
rodrigopenafurt <rodfpar@hotmail.com>
2025-04-27 10:21:49 -0300

    component nav

--- src/MedAgenda_frontend/src/components/Navbar.jsx
+++ src/MedAgenda_frontend/src/components/Navbar.jsx
@@ -7,21 +7,21 @@ const Navbar = () => {
     <div className='flex items-center justify-between text-sm py-4 mb-5'>
         <img className='w-60 cursor-pointer' src={assets.logo} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink>
+            <NavLink to='/'>
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
+            <NavLink to='/doctors'>
                 <li className='py-1'>DOUTORES</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
+            <NavLink to='/about'>
                 <li className='py-1'>SOBRE NÓS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
+            <NavLink to='/contact'>
                 <li className='py-1'>CONTATO</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-black w-3/5 m-auto hidden' />
             </NavLink>
         </ul>
         <div>
