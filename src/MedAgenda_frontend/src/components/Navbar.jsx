bfeece7071f9bda75ee4e1f425dbfbe4874a8949
Samara Mariah <samaramariah16@gmail.com>
2025-04-27 09:09:46 -0300

    Update NavBar

--- src/MedAgenda_frontend/src/components/Navbar.jsx
+++ src/MedAgenda_frontend/src/components/Navbar.jsx
@@ -4,24 +4,24 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <img src={assets.logo} alt="" />
-        <ul>
-            <NavLink>
-                <li>HOME</li>
-                <hr />
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]'>
+        <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
+            <NavLink to='/'>
+                <li className='py-1'>HOME</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
             </NavLink>
-            <NavLink>
-                <li>DOUTORES</li>
-                <hr />
+            <NavLink to='/doctors'>
+                <li className='py-1'>DOUTORES</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
             </NavLink>
-            <NavLink>
-                <li>SOBRE NÓS</li>
-                <hr />
+            <NavLink to='/about'>
+                <li className='py-1'>SOBRE NÓS</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
             </NavLink>
-            <NavLink>
-                <li>CONTATO</li>
-                <hr />
+            <NavLink to='/contact'>
+                <li className='py-1'>CONTATO</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto ' />
             </NavLink>
         </ul>
         <div>
