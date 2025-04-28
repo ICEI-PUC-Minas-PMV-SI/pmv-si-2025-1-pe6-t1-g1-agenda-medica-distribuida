309f1cc91d5cc29eac112b1d8fb3a34c13354ec9
rodrigopenafurt <rodfpar@hotmail.com>
2025-04-28 10:27:32 -0300

    useeffect funcionando

--- src/MedAgenda_frontend/src/pages/Appointment.jsx
+++ src/MedAgenda_frontend/src/pages/Appointment.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointment = () => {
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+  const [docInfo,SetDocInfo] = useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId)
+    SetDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(()=>{
+    fetchDocInfo()
+  }, [doctors,docId])
+
   return (
     <div>
-        
+      {/* detalhe medico */}
+        <div>
+          <div>
+            <img src={docInfo.image} alt="" />
+          </div>
+        </div>
     </div>
   )
 }
