74466b5943aa662d6d7d6c0dab4cc46a21fccea8
rodrigopenafurt <rodfpar@hotmail.com>
2025-04-28 16:26:02 -0300

    card doctor completo

--- src/MedAgenda_frontend/src/context/AppContext.jsx
+++ src/MedAgenda_frontend/src/context/AppContext.jsx
@@ -5,8 +5,11 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
+    const currencySymbol = 'R$'
+
     const value = {
-        doctors
+        doctors,
+        currencySymbol
     }
 
     return (
--- src/MedAgenda_frontend/src/pages/Appointment.jsx
+++ src/MedAgenda_frontend/src/pages/Appointment.jsx
@@ -7,7 +7,7 @@ import { assets } from '../assets/assets'
 const Appointment = () => {
 
   const {docId} = useParams()
-  const {doctors} = useContext(AppContext)
+  const {doctors, currencySymbol} = useContext(AppContext)
 
   const [docInfo,SetDocInfo] = useState(null)
 
@@ -26,27 +26,30 @@ const Appointment = () => {
       {/* detalhe medico */}
         <div className='flex flex-col sm:flex-row gap-20 items-center justify-center sm:justify-start'>
           <div className='pl-12'>
-            <img className='bg-gray-800 w-full sm:max-w-72 rounded-4xl' src={docInfo.image} alt="" />
+            <img className='bg-gray-800 w-full sm:max-w-35 rounded-4xl' src={docInfo.image} alt="" />
           </div>
 
           <div className='flex-l border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
             {/* doc info: nome, etc */}
             <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
               {docInfo.name} 
-              <img src={assets.verified_icon} alt="" />
+              <img className='w-5' src={assets.verified_icon} alt="" />
             </p>
-            <div>
+            <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
               <p>{docInfo.degree} - {docInfo.speciality}</p>
-              <button>{docInfo.experience}</button>
+              <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
             </div>
 
             {/* ------ sobre o medico -------- */}
             <div>
-              <p>
+              <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
                 Sobre <img src={assets.info_icon} alt="" />
               </p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim voluptatibus quidem magnam molestiae pariatur officia iusto ipsam fuga corrupti maxime nemo quod consectetur aliquid, quo porro minima suscipit itaque corporis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum suscipit harum alias laudantium magnam.  </p>
+              <p className='text-sm text-gray-500 max-w-[700px] mt-1 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim voluptatibus quidem magnam molestiae pariatur officia iusto ipsam fuga corrupti maxime nemo quod consectetur aliquid, quo porro minima suscipit itaque corporis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum suscipit harum alias laudantium magnam.  </p>
             </div>
+            <p className='text-gray-500 font-medium mt-4'>
+              Valor da consulta: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
+            </p>
           </div>
         </div>
     </div>
