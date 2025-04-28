7633698cec771e2eb5b1556e15918e803a578856
rodrigopenafurt <rodfpar@hotmail.com>
2025-04-28 18:16:57 -0300

    css datas

--- src/MedAgenda_frontend/src/pages/Appointment.jsx
+++ src/MedAgenda_frontend/src/pages/Appointment.jsx
@@ -110,17 +110,20 @@ const Appointment = () => {
         </div>
 
         {/*------- agendamento -------  */}
-        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>Horários Disponíveis</div>
-        <div>
+        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+          <p>Horários Disponíveis</p>
+          <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
           {
             docSlots.length && docSlots.map((item,index)=> (
-              <div key={index}>
+              <div className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-gray-800 text-white' : 'border border-gray-200'}`} key={index}>
                 <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
                 <p>{item[0] && item[0].datetime.getDate()}</p>
               </div>
             ))
           }
         </div>
+        </div>
+        
 
     </div>
   )
