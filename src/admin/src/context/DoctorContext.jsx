2d29425d3029180303622fda00bd9a19d944b8b6
rodrigopenafurt <rodfpar@hotmail.com>
2025-05-06 21:53:53 -0300

    context app admin e doctor

---
+++ src/admin/src/context/DoctorContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+export const DoctorContext = createContext()
+
+const DoctorContextProvider = () => {
+
+    const value = {
+
+    }
+
+    return (
+        <DoctorContext.Provider value={value}>
+            {props.children}
+        </DoctorContext.Provider>
+    )
+
+}
+
+export default DoctorContextProvider
