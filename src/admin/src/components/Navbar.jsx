d6075897959259347fc3a1e7db69cf1afee02329
Samara Mariah <samaramariah16@gmail.com>
2025-05-10 19:55:33 -0300

    update admin

--- src/admin/src/components/Navbar.jsx
+++ src/admin/src/components/Navbar.jsx
@@ -1,18 +1,28 @@
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
+import { useNavigate } from 'react-router-dom'
+
 
 const NavBar = () => {
 
-const {aToken} = useContext(AdminContext)
+const {aToken, setAToken} = useContext(AdminContext)
+
+const navigate = useNavigate()
+
+const logout = () => {
+    navigate('/')
+aToken && setAToken ('')
+aToken && localStorage.removeItem('aToken')
+}
 
   return (
     <div className='relative flex items-center justify-between text-sm py-4 mb-5'>
-        <div>
-            <img src={assets.admin_logo} alt="" />
-            <p>{aToken ? 'Admin' : 'Doctor'}</p>
+        <div className='flex items-center gap-2 text-xs'>
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
         </div>
-        <button>Logout</button>
+        <button onClick={logout} className='bg-gray-800 cursor-pointer text-white px-8 py-3 rounded-full font-light hidden md:block'>Logout</button>
     </div>
   )
 }
