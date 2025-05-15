141d9fc4fc860f376772c2f8d8c98b6017b676f4
Silviaolic <106618063+Silviaolic@users.noreply.github.com>
2025-05-14 21:40:18 -0300

    modificações

--- src/MedAgenda_mobile/app/+not-found.tsx
+++ src/MedAgenda_mobile/app/+not-found.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -10,9 +11,11 @@ export default function NotFoundScreen() {
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
+        <View style={styles.link}>
+          <Link href="/">
+            <ThemedText type="link">Go to home screen!</ThemedText>
+          </Link>
+        </View>
       </ThemedView>
     </>
   );
